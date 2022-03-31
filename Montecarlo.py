7faae2a467a41188265183ddc36fa43f4c18311f
maedekey <maeva.de.keyser@ulb.be>
2022-03-31 16:20:03 +0200

    création de méthide searchTree qui met +- en pratique l'algorithme de Montecarlo

--- Montecarlo.py
+++ Montecarlo.py
@@ -9,7 +9,7 @@ import Game
 
 
 class GameState:
-    # représente un état de jeu inclus dans un node
+    # structure qui représente un état de jeu inclus dans un node
     def __init__(self, board, player, opponent, winner=0):
         self.board = board
         self.player = player
@@ -75,22 +75,30 @@ class Tree:
         gameState = GameState(game.board, game.player1, game.player2)
 
         self.root = Node(gameState)
-        for child in self.root.children:
+        self.searchTree(self.root)
+
+    def searchTree(self, node):
+        for child in node.children:
             if not child.visited:
-                self.root.counter += 1
-                self.simulation(child)
+                node.counter += 1
+                score = self.simulation(child)
+                node.simulationRes += score
                 child.visited = True
-        self.root.fullyDeveloped = True
-        self.computeUTC(self.root)
+        node.fullyDeveloped = True
+        self.computeUTC(node)
         child = self.choseChild(self.root)
+        if len(child.children) > 0:
+            self.searchTree(child)
         # appel récursif: la simulation recommence sur child
 
     def simulation(self, node):
         path = []
-        self.simulate(path, node)
+        score = self.simulate(path, node)
+        return score
 
     def simulate(self, path, node):
         if node.gameState.winner > 0:
+            node.counter += 1
             score = self.computeScore(node)
             return score
         else:
@@ -143,26 +151,8 @@ boardGame = Board.Board(sys.argv[1])
 startGame = Game.Game("Humain", "Humain", boardGame)
 monteCarlo = Tree(startGame)
 
-# informations sur les noeuds:
-# - après rétropropagation, le noeud d'où la simulation a démarré est marqué comme visité
-# - 2 statistiques pour un noeud v:
-#   -somme des résultats des simulations rétropropagés à travers le noeud v (Q(v))
-#   -compteur du nombre de fois que le noeud v s’est retrouvé sur la traversée inverse d’une rétropropagation (N(v))
-# En d’autres termes, Q(v) reflète à quel point le noeud v est prometteur tandis que N(v) reflète à quel point le noeud
-# v a été exploré. grand Q(v) représente coup intéressant à exploiter. petit N(v) intéressant car coup peu exploré
-# ces statistiques sont mises à jour lors de la rétropropagation
-# - ces statistiques permettent de calculer l'UTC dont la formule est:
-# Q(vi)/ N(vi) + c*sqrt(ln(N(v))/N(vi)) où vi est un des noeuds internes visités, v le noeud source(ou actuel) et c =1.4
-# - quand un noeud est totalement développé, on choisit le fils ayant le meilleur UTC pour commencer la prochaine
-# simulation
-# - si ce fils est lui même complètement développé, on choisit le meilleur utc parmi ses fils et ainsi de suite
-
 # informations sur l'arbre:
-# - arbre m-aire
-# - rollout policy function qui prend en paramètre un état du jeu (noeud) et retourne le prochain coup à jouer.
 # - une simulation commence toujours au noeud qui n’a pas encore été visité précédemment
-# - quand une simulation se termine, le résultat est rétropropagé jusqu'à la racine
-# - la rétropropagation met à jour les statistiques des noeuds intermédiaires par lesquels elle passe
 # - fin de recherche: le meilleur coup est celui qui a été visité le + (donc max(N(vi)))
 
 # définitions importantes:
@@ -176,10 +166,3 @@ monteCarlo = Tree(startGame)
 # - un résultat peut être attribué à la simulation
 # - rétropropagation: traversée inverse qui commence du noeud terminal et qui remonte
 # jusqu’au noeud racine de l’arbre de jeu courant en passant par tous les noeuds intermédiaires
-
-# premier traitement:
-#    aucune simulation n'a encore été faite --> aucun fils de la racine n'est visité.
-#    On effectue une simulation sur tous les fils : 3 simulations on choisit des noeuds intermédiaires aléatoires et on
-#    s'arrête à un noeud terminal
-#    les résultats de ces 3 simulations sont propagés jusqu'à la racine
-#    le noeud racine est alors complètemennt développé
